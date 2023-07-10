285674b20d960814419cbc2a800d9532b67aea47
heondong9265 <heondong0723@gmail.com>
2023-07-10 22:02:24 +0900

    develop pull -> feat#2 기능 구현

--- character/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Characters
-
-urlpatterns = [
-    path('', Characters.as_view()),
-    path('<int:character_id>', Characters.as_view()),
-]
--- gTeamProject/urls.py
+++ gTeamProject/urls.py
@@ -1,31 +1,14 @@
-"""
-URL configuration for gTeamProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.conf import settings
-from django.urls import path, include
+from django.urls import path
 from accounts.views import RegisterView, LoginView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/question', include('question.urls')),
-    path('api/characters', include('character.urls')),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
+from django.urls import path
+from accounts.views import RegisterView, LoginView
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+]
