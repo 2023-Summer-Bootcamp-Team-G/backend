27d433b5cf53621d1e541b878abd340f830a4567
non <dojeon218@gmail.com>
2023-07-14 15:41:27 +0900

    feat: AWS Secrets Manager 연동 및 settings.py 수정

---
+++ aws.py
@@ -0,0 +1,31 @@
+import os
+import json
+import boto3
+
+
+class AWSManager:
+    _session = None
+
+    @classmethod
+    def get_session(cls):
+        if not cls._session:
+            env = os.environ.get("SERVER_NAME")
+
+            if env == "EC2":
+                cls._session = boto3.Session()
+            else:
+                cls._session = boto3.Session(
+                    region_name=os.environ.get("REGION_NAME"),
+                    aws_access_key_id=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+                    aws_secret_access_key=os.environ.get("AWS_SECRET_SECRET_KEY"),
+                )
+
+        return cls._session
+
+    @classmethod
+    def get_secret(cls, secret_name):
+        session = cls.get_session()
+        client = session.client(service_name="secretsmanager")
+
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        return json.loads(get_secret_value_response["SecretString"])
