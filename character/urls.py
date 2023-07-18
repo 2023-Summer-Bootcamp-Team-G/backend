1aeb67f4ef670915fa3d1be91321115d97896518
heondong9265 <heondong0723@gmail.com>
2023-07-19 00:51:38 +0900

    feat#21

--- character/urls.py
+++ character/urls.py
@@ -4,7 +4,6 @@ from .views import Characters, CharacterDetail, DuplicateCharacter, KeywordChart
 urlpatterns = [
     path("", Characters.as_view()),
     path("<int:character_id>", CharacterDetail.as_view()),
-    path("api/extract-phrases", nlpAPI.as_view(), name="extract-phrases"),
     path("chart", KeywordChart.as_view()),
     path("duplicate", DuplicateCharacter.as_view()),
 ]
