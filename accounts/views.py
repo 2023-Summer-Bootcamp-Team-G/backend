54afb874cc77ba85bd397f5c25c182ed9056ee37
heondong9265 <heondong0723@gmail.com>
2023-07-24 02:22:04 +0900

    Resolve conflicts in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 # authenticate는 사용자 인증을 수행하는 내장함수, 인증 자격증명(사용자 id, 비밀번호)을
 # 사용하여 사용자 인증, 인증에 성공한 경우 사용자 객체 반환, 실패한 경우 `none` 반환
 # login은 인증된 사용즈랄 로그인 처리, 세션 관리, 필요 데이터 저장해서
@@ -68,3 +69,12 @@ class LoginView(APIView):
             {"message": "Login successful."},
             status=status.HTTP_200_OK
         )
+
+
+class KakaoView(APIView):
+    def get(self, request):
+        kakao_api = "https://kauth.kakao.com/oauth/authorize?response_type=code"
+        redirect_uri = "http://127.0.0.1:8000/"
+        client_id = "c0a747f67ed13f79de992cbcdedec359"
+
+        return redirect(f"{kakao_api}&client_id={client_id}&redircet_uri={redirect_uri}")
