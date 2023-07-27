2e215d01ecc41edccd092498cc7ed85887f48f76
heondong9265 <heondong0723@gmail.com>
2023-07-28 01:26:07 +0900

    test

--- common/middleware.py
+++
@@ -1,7 +0,0 @@
-class CsrfCookieMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        request.META["HTTP_X_CSRFTOKEN"] = request.COOKIES.get("csrftoken", None)
-        return self.get_response(request)
