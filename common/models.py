3282b2053e3bebbaf87ce5b3503fa7af34968017
jueon <wndjs803@naver.com>
2023-07-28 22:48:33 +0900

    fix: BaseModel, Question get swagger 수정

--- common/models.py
+++ common/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True)
+    is_deleted = models.IntegerField(null=True, default=0)
 
     class Meta:
         abstract = True
